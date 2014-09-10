38b831d7710727bf2cc9eb0aa8e292c1e8f745d6
Jon <jon_magal@yahoo.com.br>
2014-09-10 18:27:01 -0300

    Adding the first test of similarity computing.

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ Created on 28/05/2014
 
 @author: Jonathas Magalhães
 '''
-from util.util import read_the_dataset
+from util.util import read_the_dataset, get_movie_vector
 from settings import DATASET_PATH
 
 if __name__ == '__main__':
+    movies = get_movie_vector(the_movie_file = DATASET_PATH + 'movies.dat')
     for x in range(1,2):
-        read_the_dataset(the_dataset_file = DATASET_PATH + 'r' + str(x) + '.train')
+        matrix = read_the_dataset(the_dataset_file = DATASET_PATH + 'r' + str(x) + '.train', movies = movies)
+    line = matrix[71566,:]
+    for x in line:
+        print x
--- util/util.py
+++ util/util.py
@@ -8,19 +8,19 @@ Created on 09/09/2014
 import numpy as np
 from settings import USERS, MOVIES
 
-def read_the_dataset(the_dataset_file):
+def read_the_dataset(the_dataset_file, movies):
     matrix = np.zeros((USERS, MOVIES))
-    
-    dict_movie = {}
-    value = 0
-    with file(the_dataset_file,'r') as infile:
+    with file(the_dataset_file, 'r') as infile:
         for line in infile:    
-            data = line.strip('::')
-            if data[1] in dict_movie:
-                movie_index = dict_movie.get(data[1]) 
-            else:
-                dict_movie[data[1]] = value
-                value +=1 
-            matrix[int(data[0]), movie_index] = float(data[2])  
-            
+            data = line.split('::')
+            movie_index = movies.index(data[1])
+            matrix[int(data[0])-1, movie_index] = float(data[2])              
     return matrix
+
+def get_movie_vector(the_movie_file):
+    movies = []
+    with file(the_movie_file, 'r') as infile:
+        for line in infile:    
+            data = line.split('::')
+            movies.append(data[0])
+    return movies
